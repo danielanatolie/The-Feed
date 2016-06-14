64acfed0ab372ede39af4cca2d27a39360ff4176
danielislam <daniel.anatolie@gmail.com>
2016-06-13 23:24:22 -0700

    Deselcect following

--- ParseStarterProject/TableViewController.swift
+++ ParseStarterProject/TableViewController.swift
@@ -18,8 +18,8 @@ class TableViewController: UITableViewController {
     var usernames = [""]
     // We will track user followers by using their objectId
     var userids = [""]
-    // Track who the current user is following:
-    var isFollowing = [false]
+    // Track who the current user is following dictionary:
+    var isFollowing = ["":false]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,9 +54,9 @@ class TableViewController: UITableViewController {
                                     
                                     if objects.count > 0 {
                                     
-                                        self.isFollowing.append(true) // These boolean statements will feed the [following] array
+                                        self.isFollowing[user.objectId!] = true // These boolean statements will feed the [following] array
                                         } else {
-                                        self.isFollowing.append(false)
+                                        self.isFollowing[user.objectId!] = false
                                     }
                                 }
                                 if self.isFollowing.count == self.usernames.count {
@@ -100,8 +100,9 @@ class TableViewController: UITableViewController {
         // The text in each row should be the specific user:
         cell.textLabel?.text = usernames[indexPath.row]
         
+        let followedObjectId = userids[indexPath.row]
         // We will give check-marks to the users that are in the [following] array:
-        if isFollowing[indexPath.row] == true {
+        if isFollowing[followedObjectId] == true {
              cell.accessoryType = UITableViewCellAccessoryType.Checkmark
         }
         return cell
@@ -110,21 +111,59 @@ class TableViewController: UITableViewController {
        ////////    ////////    ////////    ////////
     //*** This runs when a user taps on a cell ****//
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print(indexPath.row)
         
-        // Update table with a check-mark to indicate that current user is following already:
-        //  Get the tapped cell and add a check mark:
         var cell:UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)!
-        cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+        
+         let followedObjectId = userids[indexPath.row]
+        
+        // ** Deselect option ** // 
+        if isFollowing[followedObjectId] == false {
+            isFollowing[followedObjectId] = true
+            // Update table with a check-mark to indicate that current user is following already:
+            //  Get the tapped cell and add a check mark:
+            
+            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+            
+            
+            var following = PFObject(className: "followers")
+            // Add the tapped user-id in the following column:
+            following["following"] = userids[indexPath.row]
+            // Add the current user as a follower to the table
+            following["follower"] = PFUser.currentUser()?.objectId
+            // push to the parse server:
+            following.saveInBackground()
+        } else {
+            isFollowing[followedObjectId] = false
+            cell.accessoryType = UITableViewCellAccessoryType.None
+            // Same query from the code above:
+            
+            // ** Update the followers of the current user from parse:
+            var query = PFQuery(className: "followers")
+            // We need to make sure two conditions for the checkmarks to be updated:
+            // Search the parse online data to see if the current user [PFUser...] is a [follower] of any [following]
+            query.whereKey("follower", equalTo: PFUser.currentUser()!.objectId!)
+            // Also make sure that the person that current user is following is the actual tapped user
+            query.whereKey("following", equalTo: userids[indexPath.row])
+            
+            // The loop query will run true if the two function above where successfully satisfied:
+            // Now scan through the current users and check them off:
+            query.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
+                if let objects = objects {
+                    for objects in objects {
+                        objects.deleteInBackground()
+                    }
+                }
+            })
+            /// /// ///
+            
+            
+            
+        }
         
         
-        var following = PFObject(className: "followers")
-        // Add the tapped user-id in the following column:
-        following["following"] = userids[indexPath.row]
-        // Add the current user as a follower to the table
-        following["follower"] = PFUser.currentUser()?.objectId
-        // push to the parse server:
-        following.saveInBackground()
+        /// /// /// /// ///
+        
+        
         
         
         
